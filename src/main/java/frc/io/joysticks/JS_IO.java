1fd8d2b9c29db05d0117e95847715cbd42532ec8
JimHofm52 <JimHofmann52@gmail.com>
2020-01-17 21:02:05 -0500

    Cleaned up JS_IO and added just buttons needed so far.

--- src/main/java/frc/io/joysticks/JS_IO.java
+++ src/main/java/frc/io/joysticks/JS_IO.java
@@ -37,36 +37,16 @@ public class JS_IO{
     public static Joystick arJS[] = {leftJoystick,rightJoystick, coJoystick,
                                      gamePad, neoPad};
     // Declare all stick control
-    public static Axis rotateBot;     // Mecanum drive rotate 
-    public static Axis xDriveBot;     // Left/Right Drive
-    public static Axis yDriveBot;     // Forward/Reverse Drive
+    public static Axis leftDrive;     // Left Drive
+    public static Axis rightDrive;    // Right Drive
 
-    // Drive buttons
-    public static Button gyroResetBtn;
-    public static Button holdAngle90Btn;
-    public static Button holdAngle30Btn;
-    public static Button fieldOrientedBtn;
-    public static Button robotOrientedBtn;
-    // Hatch
-    public static Button forkBtn;       // Start fork action
-    // Climb
-    public static Button lowClimbBtn;
-    public static Button highClimbBtn;
-    public static Button fixFrontClimbBtn;
-    public static Button fixRearClimbBtn;
-    public static Button resetClimbBtn;
-    // Snorfler
-    public static Button snorflerBtn;   // Start snofle action
-    // Shaft
-    public static Button liftUpBtn;     // raise the lifter
-    // Slide
-    public static Button slideResetRightBtn;
-    public static Button slideResetLeftBtn;
-
-    // Overrides
-    public static Pov ovrSnorfPov;    //Override snorfler 0=suck, 180=spit
-    // Test-Snorfler backRolloer stall override
-    public static Button runBtn;
+    // Turret buttons
+    public static Button shooterStart;  // Start shooter motor
+    public static Button shooterStop;   // Stop motor
+    public static Button turretCW;      // Turn turret CW
+    public static Button turretCCW;     // Turn turret CCW
+    public static Button lifterUp;      // Run motor to lift balls
+    public static Button lifterDn;      // Run motor to lower balls
 
     // Constructor
     public JS_IO(){
@@ -119,210 +99,75 @@ public class JS_IO{
 
     // ----------- Normal 3 Joysticks -------------
     private static void Norm3JS(){
-        // All stick axisesssss
-        xDriveBot = new Axis(rightJoystick, 0);
-        yDriveBot = new Axis(rightJoystick, 1);
-        rotateBot = new Axis(leftJoystick, 1);
-        // Drive buttons
-        gyroResetBtn = new Button(leftJoystick, 6);
-        holdAngle90Btn = new Button(leftJoystick, 7);
-        holdAngle30Btn = new Button(leftJoystick, 9);
-        fieldOrientedBtn = new Button(leftJoystick, 8);
-        robotOrientedBtn = new Button(leftJoystick, 10);
-        // Hatch
-        forkBtn = new Button(coJoystick, 1);
-        // Climb
-        lowClimbBtn = new Button(coJoystick, 12);
-        highClimbBtn = new Button(coJoystick, 10);
-        fixFrontClimbBtn = new Button(coJoystick, 7);
-        fixRearClimbBtn = new Button(coJoystick, 8);
-        resetClimbBtn = new Button(coJoystick, 2);
-        // Snorfler
-        snorflerBtn = new Button(coJoystick, 4);
-        // Shaft
-        liftUpBtn = new Button(coJoystick, 11);
-        // Slide --- NOTE: deleted! ----
-        slideResetRightBtn = new Button(coJoystick, 5);
-        slideResetLeftBtn = new Button(coJoystick, 6);
 
-        // Overrides snorfler, 0=spit, 180=suck
-        ovrSnorfPov = new Pov(coJoystick, 0);     //0=suck, 180=spit
-        // Test rear wheel
-        runBtn = new Button(coJoystick, 9);
+        // All stick axisesssss
+        leftDrive = new Axis(leftJoystick, 0);
+        rightDrive = new Axis(rightJoystick, 0);
+
+        // Turret buttons
+        shooterStart = new Button(leftJoystick, 6);
+        shooterStop = new Button(leftJoystick, 7);
+        turretCW = new Button(leftJoystick, 9);
+        turretCCW = new Button(leftJoystick, 8);
+        lifterUp = new Button(leftJoystick, 10);
+        lifterDn = new Button(leftJoystick, 10);
     }
 
     // ----- gamePad only --------
     private static void A_GP(){
+
         // All stick axisesssss
-        xDriveBot = new Axis(gamePad, 4);
-        yDriveBot = new Axis(gamePad, 5);
-        rotateBot = new Axis(gamePad, 0);
+        leftDrive = new Axis(gamePad, 0);
+        rightDrive = new Axis(gamePad, 1);
 
-        // Drive buttons
-        gyroResetBtn = new Button(gamePad, 8);          // Back button
-        //holdAngle90Btn = new Button(gamePad, 2);        // B Button
-        holdAngle30Btn = new Button(gamePad, 1);        // A Button
-        fieldOrientedBtn = new Button(gamePad, 4);   // Y Button
-        robotOrientedBtn = new Button(gamePad, 3);   // X Button
-        // Hatch
-        forkBtn = new Button(gamePad, 5);            //Start Fork action Button
-        // Climb
-        lowClimbBtn = new Button(null, 100);
-        highClimbBtn = new Button(null, 100);
-        fixFrontClimbBtn = new Button(null, 100);
-        fixRearClimbBtn = new Button(null, 100);
-        resetClimbBtn = new Button(null, 100);
-        // Snorfler
-        snorflerBtn = new Button(gamePad, 2);           // Left Button
-        // Shaft
-        liftUpBtn = new Button(gamePad, 3);              // Right Button
-        // Slide --- NOTE: deleted! ----
-        slideResetRightBtn = new Button(null, 100);
-        slideResetLeftBtn = new Button(null, 100);
-
-        // Overrides
-        ovrSnorfPov = new Pov(gamePad, 0);
-        // Test
-        runBtn = new Button(null, 100);
+        // Turret buttons
+        shooterStart = new Button(gamePad, 6);
+        shooterStop = new Button(gamePad, 7);
+        turretCW = new Button(gamePad, 9);
+        turretCCW = new Button(gamePad, 8);
+        lifterUp = new Button(gamePad, 10);
+        lifterDn = new Button(gamePad, 10);
     }
 
     // ------------ One Joystick only -----------
     private static void A_JS(){
-        // All stick axisesssss
-        xDriveBot = new Axis(leftJoystick, 0);    // then drive fwd/bkwd
-        yDriveBot = new Axis(leftJoystick, 1);
-        rotateBot = new Axis(leftJoystick, 2);    //Rotate twist
-
-        // Drive buttons
-        gyroResetBtn = new Button(leftJoystick, 6);
-        holdAngle90Btn = new Button(leftJoystick, 7);
-        holdAngle30Btn = new Button(leftJoystick, 9);
-        fieldOrientedBtn = new Button(leftJoystick, 8);
-        robotOrientedBtn = new Button(leftJoystick, 10);
-        // Hatch
-        forkBtn = new Button(null, 100);
-        // Climb
-        lowClimbBtn = new Button(null, 100);
-        highClimbBtn = new Button(null, 100);
-        fixFrontClimbBtn = new Button(null, 100);
-        fixRearClimbBtn = new Button(null, 100);
-        resetClimbBtn = new Button(null, 100);
-        // Snorfler
-        snorflerBtn = new Button(null, 100);
-        // Shaft
-        liftUpBtn = new Button(null, 100);
-        // Slide --- NOTE: deleted! ----
-        slideResetRightBtn = new Button(null, 100);
-        slideResetLeftBtn = new Button(null, 100);
-
-        // Overrides
-        ovrSnorfPov = new Pov(leftJoystick, 0);
-        // Test
-        runBtn = new Button(null, 100);
+        CaseDefault();
     }
 
     // ----- Mixed Left Joystick & gamePad only --------
     private static void JS_GP(){
+
         // All stick axisesssss
-        xDriveBot = new Axis(leftJoystick, 0);    // then drive fwd/bkwd
-        yDriveBot = new Axis(leftJoystick, 1);
-        rotateBot = new Axis(leftJoystick, 2);    //Rotate
+        leftDrive = new Axis(gamePad, 0);
+        rightDrive = new Axis(gamePad, 1);
 
-        // Drive buttons
-        gyroResetBtn = new Button(leftJoystick, 6);
-        holdAngle90Btn = new Button(leftJoystick, 7);
-        holdAngle30Btn = new Button(leftJoystick, 9);
-        fieldOrientedBtn = new Button(leftJoystick, 8);
-        robotOrientedBtn = new Button(leftJoystick, 10);
-        // Hatch
-        forkBtn = new Button(gamePad, 7);
-        // Climb
-        lowClimbBtn = new Button(null, 100);
-        highClimbBtn = new Button(null, 100);
-        fixFrontClimbBtn = new Button(null, 100);
-        fixRearClimbBtn = new Button(null, 100);
-        resetClimbBtn = new Button(null, 100);
-        // Snorfler
-        snorflerBtn = new Button(gamePad, 5);
-        // Shaft
-        liftUpBtn = new Button(gamePad, 6);
-        // Slide --- NOTE: deleted! ----
-        slideResetRightBtn = new Button(null, 100);
-        slideResetLeftBtn = new Button(null, 100);
-
-        // Overrides
-        ovrSnorfPov = new Pov(neoPad, 0);
-        // Test
-        runBtn = new Button(null, 100);
+        // Turret buttons
+        shooterStart = new Button(gamePad, 6);
+        shooterStop = new Button(gamePad, 7);
+        turretCW = new Button(gamePad, 9);
+        turretCCW = new Button(gamePad, 8);
+        lifterUp = new Button(gamePad, 10);
+        lifterDn = new Button(gamePad, 10);
     }
 
     // ----- Nintendo gamePad only --------
     private static void NeoGP(){
-        // All stick axisesssss
-        rotateBot = new Axis(neoPad, 1);
-        xDriveBot = new Axis(neoPad, 0);
-        yDriveBot = new Axis(null, 100);
-
-        // Drive buttons
-        gyroResetBtn = new Button(neoPad, 3);
-        holdAngle90Btn = new Button(neoPad, 7);
-        holdAngle30Btn = new Button(neoPad, 1);
-        fieldOrientedBtn = new Button(neoPad, 4);
-        robotOrientedBtn = new Button(neoPad, 3);
-        // Hatch
-        forkBtn = new Button(neoPad, 2);
-        // Climb
-        lowClimbBtn = new Button(null, 100);
-        highClimbBtn = new Button(null, 100);
-        fixFrontClimbBtn = new Button(null, 100);
-        fixRearClimbBtn = new Button(null, 100);
-        resetClimbBtn = new Button(null, 100);
-        // Snorfler
-        snorflerBtn = new Button(neoPad, 3);
-        // Shaft
-        liftUpBtn = new Button(neoPad, 4);
-        // Slide
-        slideResetRightBtn = new Button(neoPad, 5);
-        slideResetLeftBtn = new Button(neoPad, 6);
-
-        // Overrides
-        ovrSnorfPov = new Pov(null, -1);   //no neoPad.getPOV();
-        // Test
-        runBtn = new Button(null, -1);
+        CaseDefault();
     }
 
     // ----------- Case Default -----------------
     private static void CaseDefault(){
+
         // All stick axisesssss
-        rotateBot = new Axis(null, 100);
-        xDriveBot = new Axis(null, 100);
-        yDriveBot = new Axis(null, 100);
+        leftDrive = new Axis(null, 0);
+        rightDrive = new Axis(null, 0);
 
-        // Drive buttons
-        gyroResetBtn = new Button(null, 100);
-        holdAngle90Btn = new Button(null, 100);
-        holdAngle30Btn = new Button(null, 100);
-        fieldOrientedBtn = new Button(null, 100);
-        robotOrientedBtn = new Button(null, 100);
-        // Hatch
-        forkBtn = new Button(null, 100);
-        // Climb
-        lowClimbBtn = new Button(null, 100);
-        highClimbBtn = new Button(null, 100);
-        fixFrontClimbBtn = new Button(null, 100);
-        fixRearClimbBtn = new Button(null, 100);
-        resetClimbBtn = new Button(null, 100);
-        // Snorfler
-        snorflerBtn = new Button(null, 100);
-        // Shaft
-        liftUpBtn = new Button(null, 100);
-        // Slide
-        slideResetRightBtn = new Button(null, 100);
-        slideResetLeftBtn = new Button(null, 100);
-
-        // Overrides
-        ovrSnorfPov =  new Pov(null, -1);
-        // Test
-        runBtn = new Button(null, 100);
+        // Turret buttons
+        shooterStart = new Button(null, 0);
+        shooterStop = new Button(null, 0);
+        turretCW = new Button(null, 0);
+        turretCCW = new Button(null, 0);
+        lifterUp = new Button(null, 0);
+        lifterDn = new Button(null, 0);
     }
 }
