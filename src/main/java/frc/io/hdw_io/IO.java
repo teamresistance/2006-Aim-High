60e769bb543d3ef4f8fa013664d98b0d10b1a7bc
JimHofm52 <JimHofmann52@gmail.com>
2020-01-20 18:25:00 -0500

    Added Color sensor & test

--- src/main/java/frc/io/hdw_io/IO.java
+++ src/main/java/frc/io/hdw_io/IO.java
@@ -2,21 +2,30 @@ package frc.io.hdw_io;
 
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
-
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import frc.io.joysticks.JS_IO;
 
- /* temp to fill with latest faults */
+import com.revrobotics.ColorSensorV3;
+
+/* temp to fill with latest faults */
 import com.ctre.phoenix.motorcontrol.*;
 import com.ctre.phoenix.motorcontrol.can.*;
 
 import com.kauailabs.navx.frc.AHRS;
 
 public class IO {
+    // navX
+    // public static NavX navX = new NavX();
+    public static AHRS ahrs;
+
+    // PDP
+    public static PowerDistributionPanel pdp = new PowerDistributionPanel(21);
 
     // Turret
     public static Victor lifter = new Victor(0);
@@ -28,18 +37,27 @@ public class IO {
     public static DigitalInput turretCCWes = new DigitalInput(0);   // CCW End Switch
     public static DigitalInput turretCWes = new DigitalInput(1);    // CC End Switch
 
-    // navX
-    // public static NavX navX = new NavX();
-    public static AHRS ahrs;
+    //---------- WoF, Color Sensor -----------------
+    /**
+    * Change the I2C port below to match the connection of your color sensor
+    */
+    private static final I2C.Port i2cPort = I2C.Port.kOnboard;
 
-    // PDP
-    public static PowerDistributionPanel pdp = new PowerDistributionPanel(21);
+    /**
+    * A Rev Color Sensor V3 object is constructed with an I2C port as a 
+    * parameter. The device will be automatically initialized with default 
+    * parameters.
+    */
+    public static ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
+    // ----------- Talon pidf control ------------------
     public static Faults _faults = new Faults(); /* temp to fill with latest faults */
     
+
     public static void init(){
         shooter.setInverted(false);
         shooter.setSensorPhase(false); // <<<<<< Adjust this to correct phasing with motor
+
     }
 
     public static void update(){
